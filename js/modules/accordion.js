3fe9332be8ba3125a66c1da57070bafc5772032e
avelino sumbane <EDM@DTBLPT309.edm.co.mz>
2026-07-21 13:21:31 +0200

    Accordion transformado em classe

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -1,14 +1,23 @@
-export default function initAccordion() {
-  const accordion = document.querySelectorAll('[data-anime="accordion"] dt');
-  if (accordion.length) {
-    accordion[0].classList.toggle("ativo");
-    accordion[0].nextElementSibling.classList.toggle("ativo");
-    function activeAccordion() {
-      this.classList.toggle("ativo");
-      this.nextElementSibling.classList.toggle("ativo");
-    }
-    accordion.forEach((item) => {
-      item.addEventListener("click", activeAccordion);
+export default class Accordion {
+  constructor(list) {
+    this.accordionList = document.querySelectorAll(list);
+    this.activeClass = "ativo";
+  }
+  toggleAccordion(item) {
+    item.classList.toggle(this.activeClass);
+    item.nextElementSibling.classList.toggle(this.activeClass);
+  }
+
+  addAccordionEvent() {
+    this.accordionList.forEach((item) => {
+      item.addEventListener("click", () => this.toggleAccordion(item));
     });
+  }
+  init() {
+    if (this.accordionList.length) {
+      this.toggleAccordion(this.accordionList[0]);
+      this.addAccordionEvent();
+    }
+    return this;
   }
 }
